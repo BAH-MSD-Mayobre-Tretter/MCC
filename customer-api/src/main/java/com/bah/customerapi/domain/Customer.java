5c2cb390662cdb1d1f50580518960f821cb87bc8
user <user@this.com>
2020-10-07 17:06:19 -0400

    created contructor variable for customer

--- customer-api/src/main/java/com/bah/customerapi/domain/Customer.java
+++ customer-api/src/main/java/com/bah/customerapi/domain/Customer.java
@@ -5,7 +5,14 @@ public class Customer {
 	private Long id;
 	private String name;
 	private String password;
+	private String email;
 	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public Long getId() {
 		return id;
 	}
@@ -23,6 +30,14 @@ public class Customer {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public Customer(Long id, String name, String password, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.email = email;
 	}
 	
 }
