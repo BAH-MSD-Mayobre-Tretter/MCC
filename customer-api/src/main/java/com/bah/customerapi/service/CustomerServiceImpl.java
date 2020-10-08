a1062ba61ee77b1ddbbf44273c78f8a0c2dc4ac7
user <user@this.com>
2020-10-07 22:02:07 -0400

    set imports and customerRepository variable for CustomerServiceImpl

--- customer-api/src/main/java/com/bah/customerapi/service/CustomerServiceImpl.java
+++ customer-api/src/main/java/com/bah/customerapi/service/CustomerServiceImpl.java
@@ -1,5 +1,16 @@
 package com.bah.customerapi.service;
 
+import java.util.Collection;
+
+import com.bah.customerapi.domain.Customer;
+import com.bah.customerapi.respository.CustomerRepository;
+
 public class CustomerServiceImpl implements CustomerService {
+	
+	private CustomerRepository customerRepository;
+	
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 }
