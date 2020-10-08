88a3e2becbe488366e31d88dc75c3f918b15b9ff
user <user@this.com>
2020-10-07 21:51:15 -0400

    Added RestController and RequestMapping annotations, imports, and set context path

--- customer-api/src/main/java/com/bah/customerapi/api/CustomerController.java
+++ customer-api/src/main/java/com/bah/customerapi/api/CustomerController.java
@@ -1,7 +1,22 @@
 package com.bah.customerapi.api;
 
+import java.util.Collection;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bah.customerapi.domain.Customer;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+	
+	@RequestMapping
+	public Collection<Customer> getAllItems() {
+		
+	}
 //	Need CRUD
 //	CREATE
 //	READ
+	
 }
