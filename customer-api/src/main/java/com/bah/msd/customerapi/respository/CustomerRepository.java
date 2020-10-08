d922c64f9d2442ad982ef9f12e91474fffe7872f
user <user@this.com>
2020-10-08 16:34:09 -0400

    updated CustomerRepository and extended it into CrudRepository

--- customer-api/src/main/java/com/bah/msd/customerapi/respository/CustomerRepository.java
+++ customer-api/src/main/java/com/bah/msd/customerapi/respository/CustomerRepository.java
@@ -1,19 +1,20 @@
 package com.bah.msd.customerapi.respository;
 
-import java.util.Collection;
+import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.stereotype.Repository;
 
 import com.bah.msd.customerapi.domain.Customer;
 
-@Repository
-public interface CustomerRepository {
-	public Customer createCustomer(Customer customer);
-	
-	public Customer findById(Long id);
-	
-	public Collection<Customer> findAll();
-	
-	public Collection<Customer> findByName(String name);
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+//	public Customer createCustomer(Customer customer);
+//	
+//	public Customer findById(Long id);
+//	
+//	public Collection<Customer> findAll();
+//	
+//	public Collection<Customer> findByName(String name);
+	boolean existsByEmail(String email);
+	boolean existsByName(String name);
 
 }
