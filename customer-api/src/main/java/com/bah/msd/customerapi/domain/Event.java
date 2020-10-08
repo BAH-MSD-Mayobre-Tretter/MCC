ab89b7496d5b9929e849a367995a3f12c02aa488
Zach Tretter <Tretter_Zachary@bah.com>
2020-10-08 16:17:26 -0400

    event domain update

--- customer-api/src/main/java/com/bah/msd/customerapi/domain/Event.java
+++ customer-api/src/main/java/com/bah/msd/customerapi/domain/Event.java
@@ -1,5 +1,46 @@
 package com.bah.msd.customerapi.domain;
 
-public class Event {
 
+
+public class Event {
+	
+	private Long id;
+	private String title;
+	private String date;
+	private String time;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public Event(Long id, String title, String date, String time) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.date = date;
+		this.time = time;
+	}
+	
 }
+
