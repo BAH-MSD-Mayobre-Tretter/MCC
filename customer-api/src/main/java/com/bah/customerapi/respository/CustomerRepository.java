c25d4f163f90c49d0b6adcfee1dbc7bc152b9a5d
user <user@this.com>
2020-10-07 21:29:35 -0400

    Added findAll and findByName methods in CustomerRepository

--- customer-api/src/main/java/com/bah/customerapi/respository/CustomerRepository.java
+++ customer-api/src/main/java/com/bah/customerapi/respository/CustomerRepository.java
@@ -1,7 +1,15 @@
 package com.bah.customerapi.respository;
 
+import java.util.*;
+
+import com.bah.customerapi.domain.Customer;
+
 public interface CustomerRepository {
-	Customer createCustomer(Customer customer);
+	public Customer createCustomer(Customer customer);
 	
-	Customer findById(Long id);
+	public Customer findById(Long id);
+	
+	public Collection<Customer> findAll();
+	
+	public Collection<Customer> findByName(String name);
 }
