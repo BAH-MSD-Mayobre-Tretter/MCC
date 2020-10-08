7293e1761565a97afe450b558293b5097a5e8cfd
user <user@this.com>
2020-10-07 22:20:29 -0400

    added imports, annotations, and created getAllItems method to CustomerController

--- customer-api/src/main/java/com/bah/customerapi/api/CustomerController.java
+++ customer-api/src/main/java/com/bah/customerapi/api/CustomerController.java
@@ -2,18 +2,25 @@ package com.bah.customerapi.api;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bah.customerapi.domain.Customer;
+import com.bah.customerapi.service.CustomerService;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 	
-	@RequestMapping
+	@Autowired
+	CustomerService customerService;
+	
+	@GetMapping
 	public Collection<Customer> getAllItems() {
-		
+		Collection<Customer> results = customerService.findAll();
+		return results;
 	}
 //	Need CRUD
 //	CREATE
