d89e57db0cbf2e92e59b1022e4a21b180620029e
user <user@this.com>
2020-10-07 21:23:13 -0400

    added hard coded customer data to CustomerRepositoryImpl

--- customer-api/src/main/java/com/bah/customerapi/respository/CustomerRepositoryImpl.java
+++ customer-api/src/main/java/com/bah/customerapi/respository/CustomerRepositoryImpl.java
@@ -1,5 +1,22 @@
 package com.bah.customerapi.respository;
 
-public class CustomerRepositoryImpl implements CustomerRepository {
+import java.util.*;
 
+import com.bah.customerapi.domain.Customer;
+
+public abstract class CustomerRepositoryImpl implements CustomerRepository {
+
+	private List<Customer> customerData = new ArrayList<>(Arrays.asList(
+			new Customer(1L, "Bob", "pass", "bob@mail.com"),
+			new Customer(2L, "Larry", "pass", "larry@mail.com"),
+			new Customer(3L, "Sally", "pass", "sally@mail.com")
+		));
+
+	public List<Customer> getCustomerData() {
+		return customerData;
+	}
+
+	public void setCustomerData(List<Customer> customerData) {
+		this.customerData = customerData;
+	}
 }
