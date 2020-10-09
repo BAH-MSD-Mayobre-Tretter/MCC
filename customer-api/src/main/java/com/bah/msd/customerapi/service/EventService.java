fc918880f45f74b1a88c926de403805d170cfe2f
Zach Tretter <Tretter_Zachary@bah.com>
2020-10-09 16:09:37 -0400

    made eventservice look like registrationservice

--- customer-api/src/main/java/com/bah/msd/customerapi/service/EventService.java
+++ customer-api/src/main/java/com/bah/msd/customerapi/service/EventService.java
@@ -1,5 +1,16 @@
 package com.bah.msd.customerapi.service;
 
+import com.bah.msd.customerapi.domain.Event;
+
 public interface EventService {
+	
+	public Iterable<Event> findall();
+	
+	public Event findById(long id);
+	
+	public Event save(Event event);
+	
+	public void deleteEvent(long id);
+	
 
 }
